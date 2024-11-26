4c0b237ab4686c441239b19c9a02ad8b940e607b
Frese Fea <fresefae@gmail.com>
2024-11-26 18:44:56 +0300

    Update Producer

--- producer/producer.go
+++ producer/producer.go
@@ -2,6 +2,7 @@ package producer
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
 
@@ -14,8 +15,18 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func main() {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+type Producer struct {
+	UserName     string
+	Password     string
+	Host         string
+	Port         string
+	QueueName    string
+	ServiceName  string
+	ExchangeName string
+}
+
+func (p *Producer) Produce(routingKey, messageType, body string) {
+	conn, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", p.UserName, p.Password, p.Host, p.Port))
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
@@ -23,26 +34,17 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
 	failOnError(err, "Failed to declare a queue")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	body := "Hello World!"
 	err = ch.PublishWithContext(ctx,
-		"",     // exchange
-		q.Name, // routing key
-		false,  // mandatory
-		false,  // immediate
+		"",         // exchange
+		routingKey, // routing key
+		false,      // mandatory
+		false,      // immediate
 		amqp.Publishing{
-			ContentType: "text/plain",
+			ContentType: messageType,
 			Body:        []byte(body),
 		})
 	failOnError(err, "Failed to publish a message")
